1ce63de856de5d78865954a36b5146fe8e9fa83a
satenikasatqa-source <satenik.asat.qa@gmail.com>
2025-11-02 19:28:07 +0400

    Homework_1_done

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,12 @@
+
+
+public class SearchTests {
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
+}
+
+
